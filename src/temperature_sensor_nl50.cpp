72f0bdc58572d8aabf05eb11ea1fedbd021d035e
arex.huang <arex@null-lab.com>
2024-08-07 19:32:31 +0800

    refine name

--- src/temperature_sensor_nl50.cpp
+++ src/temperature_sensor_nl50.cpp
@@ -12,13 +12,13 @@ namespace {
 #ifdef ARDUINO_ARCH_AVR
 // https://www.arduino.cc/reference/en/language/functions/analog-io/analogreference/
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-constexpr float kReferenceMillivolt = 1100.0;
+constexpr float kReferenceMillivolts = 1100.0;
 constexpr uint8_t kAnalogReference = INTERNAL;
 #elif defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega8__)
-constexpr float kReferenceMillivolt = 2560.0;
+constexpr float kReferenceMillivolts = 2560.0;
 constexpr uint8_t kAnalogReference = INTERNAL;
 #else
-constexpr float kReferenceMillivolt = 2560.0;
+constexpr float kReferenceMillivolts = 2560.0;
 constexpr uint8_t kAnalogReference = INTERNAL2V56;
 #endif
 #endif
@@ -32,17 +32,17 @@ void TemperatureSensorNl50::Initialize() {
 
 float TemperatureSensorNl50::Read() const {
 #if defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_ARCH_ESP32S3)
-  const float millivolt = analogReadMilliVolts(pin_);
+  const float millivolts = analogReadMilliVolts(pin_);
 #elif defined(ARDUINO_ARCH_AVR)
   const uint8_t previous_analog_reference = analog_reference;
   analog_reference = kAnalogReference;
-  const float millivolt = analogRead(pin_) * kReferenceMillivolt / 1023.0;
+  const float millivolts = analogRead(pin_) * kReferenceMillivolts / 1023.0;
   analog_reference = previous_analog_reference;
 #else
 #error "unsupported arch"
 #endif
-  // Serial.println(String("millivolt: ") + millivolt + "mv");
-  return millivolt / 10.0 - 50.0;
+  // Serial.println(String("millivolts: ") + millivolts + "mv");
+  return millivolts / 10.0 - 50.0;
 }
 
 }  // namespace emakefun
